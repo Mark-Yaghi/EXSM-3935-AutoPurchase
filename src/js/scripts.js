131a3b4ce0411e8e81bb285164fc5d4c470b62e5
Mark Yaghi <markyaghi@hotmail.com>
2022-03-30 12:31:49 -0600

    Second push, better, but still not working right. If statement line 53 not correct'

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,96 +1,91 @@
-async function main()
- {
-   
-    /*starting with small functions, and when I'm surte they're working correctly, move on to next and/or integrate them with larger program*/
+async function main() {
 
-async function checkName(nameCheck)
-    {        
+   /*starting with small functions, and when I'm surte they're working correctly, move on to next and/or integrate them with larger program*/
+
+   async function checkName(nameCheck) 
+   {
+
+      output("in the checkName function, line 8");
+      let count = nameCheck.length;
+      let validLength = false;
+      let validChar = false;
+      let revisedName = nameCheck;
+
+      do //this loop encompasses both checks - character length and correct characters. Both conditions (validChar, validLength) have to be set to true to exit the function and return to main program.
+
+      {
+         if (count > 20) //this code is to check the number opf characters...greater than 20, propmt for another name less than 21 chars long.
+         {
+
+            output("in the if because we have more than 20 letters.");
+            output("This is the count " + count);
+
+            do {
+               revisedName = await input("In the do-while:Please enter a name with less than 20 letters: ");
+               count = revisedName.length;
+
+               output("Count output in the do-while: " + count);
+               if (count <= 20) {
+                  validLength = true;
+                  output("valid length boolean value: " + validLength);
+               }
+               else {
+                  output("Too many characters.");
+                  //newName=await input("Please enter a name with less than 20 letters: ");
+
+               }
+
+            } while (validLength =false || count >= 21);
+
+         }
         
-        output("in the checkName function");
-        let count = nameCheck.length;
-        let validLength=false;     
-       let revisedName;  
-           
-       if(count>20)
-       {
-          
-             output("in the if because we have more than 20 letters.");  
-             output("This is the count "+count);
-             
 
-               do
+         else if(count<=20)      
+          {
+           output("in the elseif line 44");
+               do         //go through and make sure the name is all lower case, or uppercase or contains a hyphen. Anything else is rejected.
                {
-                  revisedName = await input("In the do-whileyaghi:Please enter a name with less than 20 letters: "); 
-                  count=revisedName.length;  
-                  
-                  //let newName= await input("Please enter a name with less than 20 letters: ");   
-                  //let newCount=newName.length;   
-                  output("Count output in the do-while"+count);   
-                  if (count<=20)
+                     output("in the do loop line 47");
+                  for (let i = 0; i <= count; i++)
                   {
-                     validLength=true;
-                                
-                  }  
-                  else
-                  {
-                     output("Too many characters.");
-                     //newName=await input("Please enter a name with less than 20 letters: ");
+                     output("'i count' in the for loop: "+ i);
+                     output("namecheck at line 52"+nameCheck);
+                     if (!(nameCheck.charcodeAt(i) >= 65 && nameCheck.charCodeAt(i) <= 90) || !(nameCheck.charCodeAt(i) >= 97 && nameCheck.charCodeAt(i) <= 122) || !(nameCheck.charCodeAt(i) = 45))
+                     {
+                        revisedName = await output("Please enter a name with less than 20 characters. Name must contain only upper case letters, lower case letters, or a hyphen: ");
 
-                  } 
-                  
-               } while(validLength!==true);//);  count>=21    
-                     
-             /* if (newCount<=20)
-            {
-              validLength=true;
-               count=newCount;                           
-            }  
-            else
-            {
-                output("Too many characters.");
-                newName=await input("Please enter a name with less than 20 letters: ");
-            } */     
-                    
-        }
+                     }
 
-        else
-        {
-            output("all good in the else ");
-        }
+                     else
+                     {
+                        validChar = true;  //correct chars, set 'validChar' to true to exit the loop                                                             
 
-        return revisedName;
-        
-       /*   if(count > 20)
-         {
-            let validName = false;
-            let newName=/*awaitinput("Please enter a name with less than 20 letters: ");            
+                     }
+                  }
+               } while (validChar = false); //if name doesn't have correct characters, do loop again
 
          }
-        
-         else if(charCode)
-         {
-            for (let i =0; i<=count; i++;)
-         }
-         else
-         {
-            output("In the else statement; number of letters is less than 20.");
+          else
+          {
 
-         }*/
-        
-       // output("count"+count);
-    }
-    
+             output("all good in the else ");
+          }     
 
-    let firstName = await input("please enter your first name: ");
-        
-    checkName(firstName); 
-    
+      } while ((validChar = false) || (validLength = false)); //if either boolean is false, loop repeats. Both must be true (chars<20, and chars letters/hypehen only) to exit function
 
-    let lastName = await input("please enter your last name: ");
-    checkName(lastName);
-    output("your name is "+firstName+" "+lastName);
+      //return revisedName;
+   }
 
-  
+
+   let firstName = await input("please enter your first name: ");
+   checkName(firstName);
+
+   let lastName = await input("please enter your last name: ");
+   checkName(lastName);
+
+   output("your name is " + firstName + " " + lastName);
+
+
 }
 
 
